06142a374aa00f4b04a7798991cf39d04c5d5edf
John Finley <jpfinley@gmail.com>
2013-01-08 20:41:54 -0500

    Coolness

---
+++ Chapter 7/John/Coolness/Coolness/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  Coolness
+//
+//  Created by John Finley on 1/8/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+//    int i;
+//    for (i = 0; i < 12; i++) {
+//        printf("Checking i = %d\n", i);
+//        if(i + 90 == i * i){
+//            break;
+//        }
+//    }
+//    printf("The answer is %d.\n", i);
+//    return 0;
+
+    int i = 0;
+    do {
+        printf("%d. John is cool\n", i);
+        i++;
+    } while (i < 12);
+    return 0;
+}
