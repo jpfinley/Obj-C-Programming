641e69dca40eebc55b4fb541d7133c1ce4583581
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-09 14:18:44 -0500

    Basic Degree conversion program

--- Chapter 5/Jeff/Degrees/Degrees/main.c
+++ Chapter 5/Jeff/Degrees/Degrees/main.c
@@ -8,11 +8,20 @@
 
 #include <stdio.h>
 
+float fahrenheitFromCelcius(float cel)
+{
+    float fahr = cel * 1.8 + 32.0;
+    printf("%f Celcius is %f Fahrenheit\n", cel, fahr);
+    return fahr;
+}
+
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    float freezeInC = 0;
+    float freezeInF = fahrenheitFromCelcius(freezeInC);
+    printf("Water freezes at %f degrees Fahrenheit\n",
+           freezeInF);
     return 0;
 }
 
