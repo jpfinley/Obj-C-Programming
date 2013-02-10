028639f2ed122243955a34d7e8e478cb46079931
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-09 21:08:55 -0500

    Current Time

--- Chapter 10/Jeff/Chapter Ten Challenge/Chapter Ten Challenge/main.c
+++ Chapter 10/Jeff/Chapter Ten Challenge/Chapter Ten Challenge/main.c
@@ -14,6 +14,10 @@ int main(int argc, const char * argv[])
 
     long secondsSince1970 = time(NULL);
     printf("it has been %ld seconds since 1970\n", secondsSince1970);
+    
+    struct tm now;
+    localtime_r(&secondsSince1970, &now);
+    printf("the time is %d:%d:%d\n", now.tm_hour, now.tm_min, now.tm_sec);
     return 0;
 }
 
