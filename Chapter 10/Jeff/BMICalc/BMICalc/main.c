b35c3f9cc8d26806835921b4ac4fbf509a58a68f
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-09 20:58:02 -0500

    Expanding for multiple people

--- Chapter 10/Jeff/BMICalc/BMICalc/main.c
+++ Chapter 10/Jeff/BMICalc/BMICalc/main.c
@@ -15,14 +15,23 @@ typedef struct {
     int weightInKilos;
 } Person;
 
+float bodyMassIndex(Person p)
+{
+    return p.weightInKilos / (p.heightInMeters * p.heightInMeters);
+}
+
 int main(int argc, const char * argv[])
 {
 
-    Person person;
-    person.weightInKilos = 96;
-    person.heightInMeters = 1.8;
-    printf("person weighs %i kilograms\n", person.weightInKilos);
-    printf("person is %.2f meters tall\n", person.heightInMeters);
+    Person personOne, personTwo;
+    personOne.weightInKilos = 96;
+    personOne.heightInMeters = 1.8;
+    personTwo.weightInKilos = 267;
+    personTwo.heightInMeters = 1.34;
+    float bmiOne = bodyMassIndex(personOne);
+    float bmiTwo = bodyMassIndex(personTwo);
+    printf("person one has a BMI of %.2f\n", bmiOne);
+    printf("person two has a BMI of %.2f\n", bmiTwo);
     return 0;
 }
 
