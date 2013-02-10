5f723a5aa60ac45cfedcbdb88fd520dce086c976
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-09 21:25:51 -0500

    Attempt at later

--- Chapter 10/Jeff/Chapter Ten Challenge/Chapter Ten Challenge/main.c
+++ Chapter 10/Jeff/Chapter Ten Challenge/Chapter Ten Challenge/main.c
@@ -18,6 +18,10 @@ int main(int argc, const char * argv[])
     struct tm now;
     localtime_r(&secondsSince1970, &now);
     printf("the time is %d:%d:%d\n", now.tm_hour, now.tm_min, now.tm_sec);
+    printf("the date is now %d-%d-%d", (now.tm_mon + 1), now.tm_mday, (now.tm_year + 1900));
+    struct tm later;
+    later = *(&now + 4000000);
+    printf("the date will be %d-%d-%d", (later.tm_mon + 1), later.tm_mday, (later.tm_year + 1900));
     return 0;
 }
 
