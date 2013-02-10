9ffe48d2fc6223575693d5219f433cf7b15b7ae8
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-09 20:38:55 -0500

    PBR File first pass

---
+++ Chapter 9/Jeff/PBR/PBR/main.c
@@ -0,0 +1,56 @@
+//
+//  main.c
+//  PBR
+//
+//  Created by Jeff Kirsch on 2/9/13.
+//  Copyright (c) 2013 jeffkirsch. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+void cartesianToPolar(float x, float y, double *rPtr, double *thetaPtr)
+{
+    // Store the radius in the supplied address
+    *rPtr = sqrt(x * x + y * y);
+    
+    //Calculate theta
+    float theta;
+    if (x == 0.0) {
+        if (y == 0.0) {
+            theta = 0.0; // technically considered undefined
+        } else if (y > 0) {
+            theta = M_PI_2;
+        } else {
+            theta = - M_PI_2;
+        }
+    } else {
+        theta = atan(y/x);
+    }
+        *thetaPtr = theta;
+}
+
+int main(int argc, const char * argv[])
+{
+
+    double pi = 3.14;
+    double integerPart;
+    double fractionPart;
+    
+    // Pass the address of integerPart as an argument
+    fractionPart = modf(pi, &integerPart);
+    
+    // Find the value stored in integerPart
+    printf("integerPart = %.0f, fractionPart = %.2f\n", integerPart, fractionPart);
+    
+    double x = 3.0;
+    double y = 4.0;
+    double radius;
+    double angle;
+    
+    cartesianToPolar(x, y, &radius, &angle);
+    printf("(%.2f, %.2f) becomes (%.2f radians, %.2f)\n", x, y, radius, angle);
+    
+    return 0;
+}
+
