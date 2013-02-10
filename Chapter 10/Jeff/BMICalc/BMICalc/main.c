e7ff0f0e2bc1af097278873c7357ad30881b01f4
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-09 20:51:48 -0500

    Redefining as type

--- Chapter 10/Jeff/BMICalc/BMICalc/main.c
+++ Chapter 10/Jeff/BMICalc/BMICalc/main.c
@@ -10,15 +10,15 @@
 
 //Here is the declaration of the struct Person
 
-struct Person {
+typedef struct {
     float heightInMeters;
     int weightInKilos;
-};
+} Person;
 
 int main(int argc, const char * argv[])
 {
 
-    struct Person person;
+    Person person;
     person.weightInKilos = 96;
     person.heightInMeters = 1.8;
     printf("person weighs %i kilograms\n", person.weightInKilos);
