8f589429d965e70116539139dbb232994c045cd6
John Finley <jfinley@barbariangroup.com>
2013-02-11 18:28:25 -0500

    Multiple Arguments

---
+++ Chapter 13/John/Multiple Arguments/Multiple Arguments/main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  Multiple Arguments
+//
+//  Created by John Finley on 2/11/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *now = [[NSDate alloc] init];
+        NSLog(@"The date is %@", now);
+        
+        double seconds = [now timeIntervalSince1970];
+        NSLog(@"It has been %f seconds since the start of 1970.", seconds);
+        
+        NSDate *later = [now dateByAddingTimeInterval:100000];
+        NSLog(@"In 100,000 seconds it will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSUInteger day = [cal ordinalityOfUnit:NSDayCalendarUnit
+                                        inUnit:NSMonthCalendarUnit
+                                       forDate:now];
+        NSLog(@"This is the %ld day of the month.", (unsigned long)day);
+        
+    }
+    return 0;
+}
+
