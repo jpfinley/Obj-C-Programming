bbd71e709fb55cd2de061547a0ce8055bb16968d
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-16 09:25:46 -0500

    Playing with units

--- Chapter 12/Jeff/TimeAfterTime/TimeAfterTime/main.m
+++ Chapter 12/Jeff/TimeAfterTime/TimeAfterTime/main.m
@@ -13,13 +13,19 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        NSDate *now = [NSDate date];
+        NSDate *now = [[NSDate alloc] init];
         NSLog(@"The new date lives at %@", now);
         double seconds = [now timeIntervalSince1970];
         NSLog(@"It has been %f seconds since the start of 1970.", seconds);
         
         NSDate *later = [now dateByAddingTimeInterval:100000];
         NSLog(@"In 100,000 seconds it will be %@", later);
+        
+        NSCalendar *cal = [NSCalendar currentCalendar];
+        NSUInteger day = [cal ordinalityOfUnit:NSHourCalendarUnit
+                                        inUnit:NSYearCalendarUnit
+                                       forDate:now];
+        NSLog(@"This is the %lu hour of the year", day);
         
     }
     return 0;
