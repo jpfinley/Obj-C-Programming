a8f274265d48eaef6a637117dea7d4fd12f9edc3
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-16 08:48:30 -0500

    Initial Time project

---
+++ Chapter 12/Jeff/TimeAfterTime/TimeAfterTime/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  TimeAfterTime
+//
+//  Created by Jeff Kirsch on 2/16/13.
+//  Copyright (c) 2013 jeffkirsch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSDate *now = [NSDate date];
+        NSLog(@"The new date lives at %p", now);
+        
+    }
+    return 0;
+}
+
