e4ac7defbef076dcbefcbaf93a577b7d729bf2d1
John Finley <jpfinley@gmail.com>
2013-02-16 10:45:20 -0500

    Updating Chapter 13 challenge to be more Objective-C-ish

--- Chapter 13/John/Chapter 13 Challenge/Chapter 13 Challenge/main.m
+++ Chapter 13/John/Chapter 13 Challenge/Chapter 13 Challenge/main.m
@@ -18,7 +18,7 @@ int main(int argc, const char * argv[])
         NSLog(@"The current time zone is %@.", currentTimeZone);
         
         // Printing out the Daylight Savings with an if-then switch
-        if (currentTimeZone.isDaylightSavingTime) {
+        if ([currentTimeZone isDaylightSavingTime]) {
             NSLog(@"And, YES, we are currently in Daylight Savings.");
         } else {
             NSLog(@"And, NO, we are not currently in Daylight Savings.");
@@ -26,7 +26,7 @@ int main(int argc, const char * argv[])
         
         // Printing out the Daylight Savings based on the boolean
         // Tried to cast the BOOL as a string but didn't have documentation available.
-        NSLog(@"Are we currently in Daylight Savings? %d", currentTimeZone.isDaylightSavingTime);
+        NSLog(@"Are we currently in Daylight Savings? %d", [currentTimeZone isDaylightSavingTime]);
         
     }
     return 0;
