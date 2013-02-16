bd8a9b48a91ee87bac22d93ccc4d64a772846c57
Jeff Kirsch <jeff@jeffkirsch.com>
2013-02-16 17:31:59 -0500

    Challenge attempt
    Currently matching on each member of the string

---
+++ Chapter 15/Jeff/Challenges/Common Names/Common Names/main.m
@@ -0,0 +1,50 @@
+//
+//  main.m
+//  Common Names
+//
+//  Created by Jeff Kirsch on 2/16/13.
+//  Copyright (c) 2013 jeffkirsch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int count = 0;
+        
+        // Read in both files as strings
+        NSString *properNamesString = [NSString stringWithContentsOfFile:@"/usr/share/dict/propernames"
+                                                          encoding:NSUTF8StringEncoding
+                                                             error:NULL
+                                 ];
+        NSString *wordsString = [NSString stringWithContentsOfFile:@"/usr/share/dict/words"
+                                                          encoding:NSUTF8StringEncoding
+                                                             error:NULL
+                                 ];
+        
+        // Break into arrays
+        NSArray *properNames = [properNamesString componentsSeparatedByString:@"\n"];
+        NSArray *words = [wordsString componentsSeparatedByString:@"\n"];
+        
+        // Go through the first array one at a time to look for matches to the second
+        
+        for (NSString *p in properNames) {
+            for (NSString *w in words) {
+                NSRange r = [p rangeOfString:w
+                                     options:NSCaseInsensitiveSearch];
+                if (r.location != NSNotFound) {
+                    count++;
+                    NSLog(@"%@ matches to %@.", p, w);
+                }
+            }
+        }
+        
+        NSLog(@"The total number of matches is %d", count);
+        
+    }
+    return 0;
+}
+
