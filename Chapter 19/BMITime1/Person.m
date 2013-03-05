f145adf29394cebdeca3ddd8548f212abd61193f
John Finley <jpfinley@gmail.com>
2013-03-05 09:05:48 -0500

    Bring over BMITime from chapter 18

---
+++ Chapter 19/BMITime1/Person.m
@@ -0,0 +1,21 @@
+//
+//  Person.m
+//  BMITime
+//
+//  Created by John Finley on 3/1/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@synthesize heightInMeters, weightInKilos;
+
+- (float)bodyMassIndex
+{
+    float h = [self heightInMeters];
+    return [self weightInKilos] / (h * h);
+}
+
+@end
---
+++ Chapter 19/John/BMITime/BMITime/Employee.h
@@ -0,0 +1,18 @@
+//
+//  Employee.h
+//  BMITime
+//
+//  Created by John Finley on 3/3/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Person.h"
+
+@interface Employee : Person
+{
+    int employeeID;
+}
+@property int employeeID;
+
+@end
