93b74e0a55ffed862cebba360dc30556c4cdf2bd
John Finley <jpfinley@gmail.com>
2013-06-08 14:47:44 -0400

    Moving things around. I'm a jerk

---
+++ Chapter 24/John/Callbacks/Callbacks/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by John Finley on 6/8/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+
+        // insert code here...
+        NSLog(@"Hello, World!");
+
+    }
+    return 0;
+}
