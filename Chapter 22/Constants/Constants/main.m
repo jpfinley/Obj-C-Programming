bdd3f94e87932d3b5dd2ea3bcf24aeb2bd8bc695
John Finley <jpfinley@gmail.com>
2013-06-08 13:29:47 -0400

    Chapter 22

--- Chapter 22/Constants/Constants/main.m
+++ Chapter 22/Constants/Constants/main.m
@@ -13,8 +13,13 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
 
-        // insert code here...
-        NSLog(@"Hello, World!");
+        // In literal NSString, use \u for arbitrary unicode chars
+        NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10, 12));
+
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@", currency);
 
     }
     return 0;
