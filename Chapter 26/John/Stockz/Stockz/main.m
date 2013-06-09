80eb46577640b8dae3237e55e1ba405df1bcfc5b
John Finley <jpfinley@gmail.com>
2013-06-08 22:06:18 -0400

    Dictionaries

---
+++ Chapter 26/John/Stockz/Stockz/main.m
@@ -0,0 +1,46 @@
+//
+//  main.m
+//  Stockz
+//
+//  Created by John Finley on 6/8/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSMutableArray *stocks = [[NSMutableArray alloc] init];
+        
+        NSMutableDictionary *stock;
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"AAPL"
+                  forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:200]
+                  forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        stock = [NSMutableDictionary dictionary];
+        [stock setObject:@"GOOG"
+                  forKey:@"symbol"];
+        [stock setObject:[NSNumber numberWithInt:160]
+                  forKey:@"shares"];
+        [stocks addObject:stock];
+        
+        [stocks writeToFile:@"/tmp/stocks.plist"
+                 atomically:YES];
+        
+        NSArray *stockList = [NSArray arrayWithContentsOfFile:@"/tmp/stocks.plist"];
+        
+        for (NSDictionary *d in stockList) {
+            NSLog(@"I have %@ shares of %@", [d objectForKey:@"shares"], [d objectForKey:@"symbol"]);
+        }
+        
+    }
+    return 0;
+}
+
