7fce3e741ed627ccf30f06118dd2f78331337df0
John Finley <jpfinley@gmail.com>
2013-06-08 22:56:09 -0400

    Generated

---
+++ Chapter 27/John/iTodo/iTodo/JFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JFAppDelegate.h
+//  iTodo
+//
+//  Created by John Finley on 6/8/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Chapter 27/John/iTodo/iTodo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iTodo
+//
+//  Created by John Finley on 6/8/13.
+//  Copyright (c) 2013 John Finley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JFAppDelegate class]));
+    }
+}
